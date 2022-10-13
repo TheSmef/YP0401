70485c392996af42d03e75a2af9df61ba2f92a26
Соколов Александр Анатольевич <sokolov19868@gmail.com>
2022-10-13 15:33:15 +0300

    PR1

---
+++ YP/src/test/java/com/example/YP/YpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
